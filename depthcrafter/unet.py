f1edf755510e45b9a1cac58f3c6c5a446a5101c0
YO-shimi-NE <80862747+YO-shimi-NE@users.noreply.github.com>
2025-01-23 21:09:11 -0300

    test

--- depthcrafter/unet.py
+++ depthcrafter/unet.py
@@ -21,6 +21,9 @@ class DiffusersUNetSpatioTemporalConditionModelDepthCrafter(
         added_time_ids: torch.Tensor,
         return_dict: bool = True,
     ) -> Union[UNetSpatioTemporalConditionOutput, Tuple]:
+        print("unet forward start")
+        print(f"sample size: {sample.size()} timestep: {timestep.size()} encoder_hidden_states: {encoder_hidden_states.size()} added_time_ids: {added_time_ids.size()}")
+        print("unet forward end")
 
         # 1. time
         timesteps = timestep
